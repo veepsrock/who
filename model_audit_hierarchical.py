61eb8653eccaf4ff8943579ce0c86bd3f8eb7b8e
veepsrock <veepsrock>
2023-11-03 20:03:33 +0000

    model eval at broad theme level

---
+++ model_audit_hierarchical.py
@@ -0,0 +1,147 @@
+# Databricks notebook source
+import pandas as pd
+import re
+import json
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.metrics import f1_score
+from collections import defaultdict
+pd.set_option("display.max_columns" , 50)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Read in data
+
+# COMMAND ----------
+
+df = pd.read_pickle("./audit_model_training_data.pkl")
+df["themeIds"] = df['themeIds'].apply(lambda themes: [theme for theme in themes if theme != 'rfi'] if isinstance(themes, list) else themes)
+df["themeIds"]=df["themeIds"].apply(lambda x: x[:2] if isinstance(x, list) else x)
+
+# COMMAND ----------
+
+theme_dict= {
+    "conspiracy-corruption": ["bioweapon", "conspiracy", "corruption", "media-bias", "medical-exploitation"],
+    "illness-cause": ["stigmatization", "case-reporting", "symptoms-severity", "variants"],
+    "intervention-capacity":["capacity"],
+    "prevention-treatment-alternative": ["alternative-cures"],
+    "prevention-treatment-approved": ["alternative-cures","prevention-collective", "prevention-individual", "treatment", "vaccine-efficacy", "vaccine-side-effects"]
+}
+
+# COMMAND ----------
+
+df.dropna(subset=["themeIds", "themeIdsReviewed"], inplace = True)
+
+# COMMAND ----------
+
+def map_themes(themes):
+    if isinstance(themes, list):
+        big_themes = []
+        for theme in themes:
+            for key, values in theme_dict.items():
+                if theme in values:
+                    big_themes.append(key)
+                    break
+        return big_themes if big_themes else None
+    return None
+
+# COMMAND ----------
+
+df['themeIdsReviewedParent'] = df['themeIdsReviewed'].apply(map_themes)
+df['themeIdsParent'] = df['themeIds'].apply(map_themes)
+
+# COMMAND ----------
+
+df.dropna(subset=["themeIdsParent", "themeIdsReviewedParent"], inplace = True)
+
+# COMMAND ----------
+
+all_labels = list(theme_dict.keys())
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Transform
+
+# COMMAND ----------
+
+mlb = MultiLabelBinarizer()
+mlb.fit([all_labels])
+
+# COMMAND ----------
+
+y_true = mlb.transform(df["themeIdsReviewedParent"])
+y_pred = mlb.transform(df["themeIdsParent"])
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC # Perform for original
+
+# COMMAND ----------
+
+theme_dict_child= {
+    "bioweapon":["bioligical weapon", "chemical agent"],
+    "conspiracy": ["conspiracy", "nefarious plots"],
+    "corruption": ["corruption", "economic exploitation", "profiteering","extortion"],
+    "media-bias": ["media slant and bias", "fake news"],
+    "medical-exploitation": ["medical exploitation", "experimental treatments", "expired medicine", "guinea pigs"],
+    "variants": ["disease variants", "disease genetic modifications"],
+    "alternative-cures": ["alternative cures", "herbal remedies", "home remedies", "healers and healing"],
+    "prevention-collective":["collective prevention", "lockdowns", "travel bans", "travel restrictions"],
+    "prevention-individual":["individual prevention", "non-pharmaceutical interventions", "quarantine", "face masks", "hand washing"],
+    "capacity":["capacity of public health system (hospitals, doctors, governments, aid)"],
+    "religious-practices":["religious belief", "religious leaders", "cultural practices"],
+    "treatment":["pharmaceutical treatment", "clinical treatment", "pills"],
+    "vaccine-efficacy":["vaccine efficacy", "vaccines"],
+    'case-reporting':[],
+    'stigmatization':[],
+    'symptoms-severity': [], 
+    'vaccine-side-effects':[]
+}
+
+# COMMAND ----------
+
+all_labels_child = list(theme_dict_child.keys())
+
+# COMMAND ----------
+
+mlb_child = MultiLabelBinarizer()
+mlb_child.fit([all_labels_child])
+
+# COMMAND ----------
+
+y_true_child = mlb_child.transform(df["themeIdsReviewed"])
+y_pred_child = mlb_child.transform(df["themeIds"])
+
+# COMMAND ----------
+
+macro_scores, micro_scores = defaultdict(list), defaultdict(list)
+
+
+# COMMAND ----------
+
+macro_scores["Zero Shot Parent"] = [f1_score(y_true, y_pred, average='macro')]
+micro_scores["Zero Shot Parent"] = [f1_score(y_true, y_pred, average='micro')]
+
+# COMMAND ----------
+
+macro_scores["Zero Shot Child"] = [f1_score(y_true_child, y_pred_child, average='macro')]
+micro_scores["Zero Shot Child"] = [f1_score(y_true_child, y_pred_child, average='micro')]
+
+# COMMAND ----------
+
+pd.DataFrame(macro_scores).T
+
+# COMMAND ----------
+
+pd.DataFrame(micro_scores).T
+
+# COMMAND ----------
+
+
