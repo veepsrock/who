c1c5bc76171876b9b63f7c832ff6d6bce27f2314
veepsrock <veepsrock>
2023-11-09 19:19:05 +0000

    adding precision and recall scores

--- zero_shot_scores.py
+++ zero_shot_scores.py
@@ -7,7 +7,7 @@ import sys
 import numpy as np
 import pickle
 from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, precision_score, recall_score
 from collections import defaultdict
 import mlflow
 from mlflow.entities.run import Run
@@ -35,8 +35,10 @@ def mlb_transform(label_list, true_col, pred_col, num_labels):
     # get scores
     macro_score = f1_score(y_true, y_pred, average='macro')
     micro_score = f1_score(y_true, y_pred, average='micro')
+    precision = precision_score(y_true, y_pred, average='weighted')
+    recall = recall_score(y_true, y_pred, average='weighted')
 
-    return macro_score, micro_score
+    return macro_score, micro_score, precision, recall
 
 # COMMAND ----------
 
@@ -48,7 +50,7 @@ def run_zs_experiment(num_labels):
     mlflow.set_experiment(exp_name)
     run_name = "zero_shot_run"
     parent_run = mlflow.start_run(run_name = run_name, nested = True)
-    
+
     # get themes
     for taxonomy_type in ["zero_shot_parent", "zero_shot_child"]:
         if taxonomy_type == "zero_shot_parent":
@@ -57,7 +59,7 @@ def run_zs_experiment(num_labels):
             # create labels list
             all_labels_parent = list(theme_dict.keys())
             # get scores
-            macro_score_parent, micro_score_parent = mlb_transform(all_labels_parent, "themeIdsReviewedParent", "themeIdsParent", 2)
+            macro_score_parent, micro_score_parent, precision_parent, recall_parent = mlb_transform(all_labels_parent, "themeIdsReviewedParent", "themeIdsParent", 2)
 
         else:
             with open("theme_dict.json", 'r') as f:
@@ -66,11 +68,11 @@ def run_zs_experiment(num_labels):
             all_labels = list(theme_dict.keys())
 
             # get scores
-            macro_score, micro_score = mlb_transform(all_labels, "themeIdsReviewed", "themeIds", 2)
+            macro_score, micro_score, precision, recall = mlb_transform(all_labels, "themeIdsReviewed", "themeIds", 2)
 
     # run experiment
     mlflow.log_params({"taxonomy_type": taxonomy_type, "number_of_labels": num_labels})
-    mlflow.log_metrics({"macro_f1_parent": macro_score_parent, "micro_f1_parent": micro_score_parent, "macro_f1_child": macro_score, "micro_f1_child": micro_score})    
+    mlflow.log_metrics({"macro_f1_parent": macro_score_parent, "micro_f1_parent": micro_score_parent, "macro_f1_child": macro_score, "micro_f1_child": micro_score, "precision_parent": precision_parent, "recall_parent": recall_parent, "precision_child": precision, "recall_child": recall})    
     
     # end run
     mlflow.end_run()
